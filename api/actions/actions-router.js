0eb27ae747547536158698dd387101d9fefde3f9
ChristoherHenao <cq.henao@gmail.com>
2021-09-03 12:05:21 -0400

    routers basic structure done and wired

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,28 @@
 // Write your "actions" router here!
+const express = require('express')
+const Actions = require('./actions-model')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    res.json('get actions is working')
+})
+
+router.get('/:id', (req, res, next) => {
+    res.json('get actions by id is working')
+})
+
+router.post('/', (req, res, next) => {
+    res.json('post actions is working')
+})
+
+router.put('/:id', (req, res, next) => {
+    res.json('put actions is working')
+})
+
+router.delete('/:id', (req, res, next) => {
+    res.json('delete actions is working')
+})
+
+
+module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,32 @@
 // Write your "projects" router here!
+const express = require('express')
+const Projects = require('./projects-model')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    res.json("get projects is working")
+})
+
+router.get('/:id', (req, res, next) => {
+    res.json("get projects by id is working")
+})
+
+router.post('/', (req, res, next) => {
+    res.json("post projects is working")
+})
+
+router.put('/:id', (req, res, next) => {
+    res.json("put projects is working")
+})
+
+router.delete('/:id', (req, res, next) => {
+    res.json("delete projects is working")
+})
+
+router.get('/:id/actions', (req, res, next) => {
+    res.json("get project/:id/ actions is working")
+})
+
+
+module.exports = router
